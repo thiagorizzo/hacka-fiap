3a39e91585a63122708fee925ba0ae632c1a66ff
danilo_felix <danilo_fmore@hotmail.com>
2025-02-06 20:24:44 -0300

    HashImage

--- src/services/email_service.py
+++ src/services/email_service.py
@@ -3,10 +3,17 @@ import smtplib
 from email.message import EmailMessage
 import mimetypes
 import os
+from PIL import Image
+import imagehash
+
 
 class EmailService:
     @inject
     def __init__(self):
+        self.last_image_hash = None
+        self.qtdeEnviada = 0
+        self.qtdeTotal = 0
+        self.qtdeSimilar = 0
         self.email_sender = None
         self.email_receiver = None
         self.email_password = None
@@ -47,6 +54,11 @@ class EmailService:
     def send_email_with_image(self, image_file: str):
         try:
             full_image_path = os.path.join(self.resources_images_path, image_file)
+
+            if self.is_similar_image(full_image_path):
+
+                return  # Imagem é repetida, não envia o e-mail
+
             #self.__send_email(full_image_path)
             print(f'Email enviado para {self.email_receiver} com sucesso.')
         except Exception as e:
@@ -63,3 +75,33 @@ class EmailService:
             return (False, "Falha na autenticação. Verifique seu e-mail e senha de app.")
         except smtplib.SMTPException as e:
             return (False, f"Erro ao conectar ao servidor SMTP: {e}")
+        
+
+
+    def is_similar_image(self, image_path, threshold=5):
+        """Verifica se a nova imagem é muito semelhante à última enviada"""
+        if not os.path.exists(image_path):
+            return False
+        print(f"Total: {self.qtdeTotal} - Similar: {self.qtdeSimilar} - Enviadas {self.qtdeEnviada}")
+       
+        self.qtdeTotal += 1
+       
+        new_hash = imagehash.phash(Image.open(image_path))  # Gera o hash da nova imagem
+
+        if self.last_image_hash is None:
+            self.qtdeEnviada +=1; 
+            self.last_image_hash = new_hash
+            return False  # Primeira imagem, então pode enviar
+
+        # Calcula a diferença entre os hashes (distância de Hamming)
+        hash_difference = self.last_image_hash - new_hash
+
+        if hash_difference <= threshold:
+            self.qtdeSimilar +=1
+            print(f"Imagem muito semelhante à anterior (diferença: {hash_difference}), e-mail não enviado.")
+            return True  # A imagem é muito parecida, não precisa enviar
+
+        # Atualiza o último hash enviado
+        self.qtdeEnviada +=1; 
+        self.last_image_hash = new_hash
+        return False  # Imagem diferente, pode enviar
